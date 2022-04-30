8c3cb80b76dc17cffe7ec837ac9f27c0620622c4
Exterminatus295 <81161457+Exterminatus295@users.noreply.github.com>
2022-05-01 02:17:32 +0300

    Ermolin

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,18 @@
 #include <string.h>
 
 int main() {
-	Automata first = Automata();
-	first.On();
-	first.AddCoin(500);
-	int _position = 1;
-	first.ChoiceMenu(_position);
-	first.Cook();
-	first.Finish(_position);
-	bool correct = false;
-	if (1 == first.GetState()) {
-		correct = true;
-		std::cout << correct;
-	} else {
-		std::cout << correct;
-	}
+    Automata first = Automata();
+    first.On();
+    first.AddCoin(500);
+    int _position = 1;
+    first.ChoiceMenu(_position);
+    first.Cook();
+    first.Finish(_position);
+    bool correct = false;
+    if (1 == first.GetState()) {
+	    correct = true;
+	    std::cout << correct;
+    } else {
+	    std::cout << correct;
+    }
 }
